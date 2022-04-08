9923c870ed107c92e34c354ce9e7fa1c85d54956
Raghav Gupta <42888633+Gupta-Raghav@users.noreply.github.com>
2022-04-08 18:16:39 -0400

    Time: 8 ms (35.19%), Space: 9.6 MB (5.80%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int smaller = -1;
+        int smaller = 0;
         for(int i=matrix.size()-1;i>=0;i--){
             if(matrix[i][matrix[0].size()-1]>target){
                 smaller = i; 
@@ -10,12 +10,10 @@ public:
                 return true;
             }
             }
-        if(smaller!=-1){
         for(int j=0;j<matrix[0].size();j++){
             if(matrix[smaller][j]==target){
                 return true;
             }
-        }
         }
          return false;
         
